1aeed45106d51c611e3bd4cdb6b2cd58fea1599f
sanjiv22 <sanjivshrestha22@gmail.com>
2017-04-18 11:39:01 +0545

    created form

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'articles/new'
-
-  get 'articles/create'
-
-  get 'articles/index'
-
-  get 'articles/show'
-
-  #change the lines below
+  
+  #tells the routes about our new controller
+  resources :articles
+  
   #get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
