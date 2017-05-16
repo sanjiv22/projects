87adc380124da1f4e8b6f78c18fb8eea207bc157
sanjiv22 <sanjivshrestha22@gmail.com>
2017-05-16 11:19:10 +0545

    adds show and redirect

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   	@article = Article.new(article_params)
 
   	@article.save
-  	redirect_to new_article_path
+  	redirect_to @article
 
   end
 
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
+  	@article = Article.find(params[:id])
   end
 
   private 
