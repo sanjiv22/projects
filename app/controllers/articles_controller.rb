c1ba94fa51502a5ee31f90047c3c7a7d1387ff0a
sanjiv22 <sanjivshrestha22@gmail.com>
2017-05-23 11:47:43 +0545

    added edit update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,41 @@
 class ArticlesController < ApplicationController
-  def new
+	def new
+		@article = Article.new
+	end
 
-  end
+	def edit
+		@article = Article.find(params[:id])
+	end
 
-  def create
-  	@article = Article.new(article_params)
+	def create
+		@article = Article.new(article_params)
 
-  	@article.save
-  	redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+	def update
+		@article = Article.find(params[:id]) 
 
-  end
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
 
-  def index
-  end
+	def index
+		@articles = Article.all() 
+	end
 
-  def show
-  	@article = Article.find(params[:id])
-  end
+	def show
+		@article = Article.find(params[:id])
+	end
 
-  private 
-  def article_params
-  	params.require(:article).permit(:title, :text)
-  end
-
+	private 
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
 end
